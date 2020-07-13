3b56d099a97e6f3ad1f10fd2f8f15ff45c6b6bd4
Spyros Kyritsis <spyros.kyritsis@gmail.com>
2020-07-13 10:28:14 +0200

    first commit

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', (req, res, next)=> {
+  res.render('index');
+});
+
+router.get('/signin', (req, res, next) =>{
+  res.render('signin');
+});
+
+router.get('/signup', (req, res, next) =>{
+  res.render('signup');
+});
+
+router.get('/missions', (req, res, next) =>{
+  res.render('missions');
+});
+
+module.exports = router;
